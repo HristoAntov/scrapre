d875a86b2d8c39daf5dce97fe1bd66ec37120a40
Hristo Antov <hristo@antov.dev>
2024-06-15 12:24:24 +0200

    Implement tutorial #1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Pokemon\PokemonGenerationScraperController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/pokemon/generation', PokemonGenerationScraperController::class)
+    ->name('pokemon.generation');
